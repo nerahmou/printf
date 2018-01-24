b84267178c9d242a5d12afa320b2aaa3e71f5eef
Nessim Rahmoun <nerahmou@z2r1p2.le-101.fr>
2018-01-24 11:08:11 +0100

    modif print_d

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nerahmou <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/15 20:58:30 by nerahmou     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/23 13:47:35 by nerahmou    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/24 11:04:12 by nerahmou    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,10 +18,188 @@
 
 int main(int argc, const char *argv[])
 {
-	int i =0;
-	//printf("salut%*n\n", &i);
-	ft_printf("salut {{bold}%-5b{eoc}\n", 5);
+	printf("{% 03d}\n", 0);
+	ft_printf("{% 03d}\n", 0);
+/*
+	printf("Vrai printf : \n\n");
+	printf("|%i|\n", -42);
+	printf("|%1i|\n", -42);
+	printf("|%2i|\n", -42);
+	printf("|%.i|\n", -42);
+	printf("|%.1i|\n", -42);
+	printf("|%.2i|\n", -42);
+	printf("|%1.i|\n", -42);
+	printf("|%2.1i|\n", -42);
+	printf("|%2.2i|\n", -42);
+	printf("|%01i|\n", -42);
+	printf("|%02.i|\n", -42);
+	printf("|%02.1i|\n", -42);
+	printf("|%02.2i|\n\n", -42);
+	printf("|%+ i|\n", -42);
+	printf("|%+ 1i|\n", -42);
+	printf("|%+ 2i|\n", -42);
+	printf("|%+ .i|\n", -42);
+	printf("|%+ .1i|\n", -42);
+	printf("|%.3i|\n", -42);
+	printf("|%+ 1.i|\n", -42);
+	printf("|%+ 2.1i|\n", -42);
+	printf("|%+ 2.2i|\n", -42);
+	printf("|%+ 01i|\n", -42);
+	printf("|%+ 02i|\n", -42);
+	printf("|%+ 02.1i|\n", -42);
+	printf("|%+ 02.2i|\n\n", -42);
+
 	
-	printf("%d", i);
+	printf("\n\nFt_printf : \n\n");
+	
+	ft_printf("|%i|\n", -42);
+	ft_printf("|%1i|\n", -42);
+	ft_printf("|%2i|\n", -42);
+	ft_printf("|%.i|\n", -42);
+	ft_printf("|%.1i|\n", -42);
+	ft_printf("|%.2i|\n", -42);
+	ft_printf("|%1.i|\n", -42);
+	ft_printf("|%2.1i|\n", -42);
+	ft_printf("|%2.2i|\n", -42);
+	ft_printf("|%01i|\n", -42);
+	ft_printf("|%02.i|\n", -42);
+	ft_printf("|%02.1i|\n", -42);
+	ft_printf("|%02.2i|\n\n", -42);
+	ft_printf("|%+ i|\n", -42);
+	ft_printf("|%+ 1i|\n", -42);
+	ft_printf("|%+ 2i|\n", -42);
+	ft_printf("|%+ .i|\n", -42);
+	ft_printf("|%+ .1i|\n", -42);
+	ft_printf("|%.3i|\n", -42);
+	ft_printf("|%+ 1.i|\n", -42);
+	ft_printf("|%+ 2.1i|\n", -42);
+	ft_printf("|%+ 2.2i|\n", -42);
+	ft_printf("|%+ 01i|\n", -42);
+	ft_printf("|%+ 02i|\n", -42);
+	ft_printf("|%+ 02.1i|\n", -42);
+	ft_printf("|%+ 02.2i|\n\n", -42);
+	
+	printf("Vrai printf : \n\n");
+	printf("|%i|\n", -42);
+	printf("|%1i|\n", -42);
+	printf("|%2i|\n", -42);
+	printf("|%.i|\n", -42);
+	printf("|%.1i|\n", -42);
+	printf("|%.2i|\n", -42);
+	printf("|%1.i|\n", -42);
+	printf("|%2.1i|\n", -42);
+	printf("|%2.2i|\n", -42);
+	printf("|%3.2i|\n", -42);
+	printf("|%01i|\n", -42);
+	printf("|%02.i|\n", -42);
+	printf("|%02.1i|\n", -42);
+	printf("|%02.2i|\n\n", -42);
+	printf("|%+ i|\n", -42);
+	printf("|%+ 1i|\n", -42);
+	printf("|%+ 2i|\n", -42);
+	printf("|%+ .i|\n", -42);
+	printf("|%+ .1i|\n", -42);
+	printf("|%.3i|\n", -42);
+	printf("|%+ 1.i|\n", -42);
+	printf("|%+ 2.1i|\n", -42);
+	printf("|%+ 2.2i|\n", -42);
+	printf("|%+ 3.2i|\n", -42);
+	printf("|%+ 2.3i|\n", -42);
+	printf("|%+ 01i|\n", -42);
+	printf("|%+ 02i|\n", -42);
+	printf("|%+ 02.1i|\n", -42);
+	printf("|%+ 02.2i|\n\n",-42 );
+
+	
+	printf("\n\nFt_printf : \n\n");
+	ft_printf("|%i|\n", -42);
+	ft_printf("|%1i|\n", -42);
+	ft_printf("|%2i|\n", -42);
+	ft_printf("|%.i|\n", -42);
+	ft_printf("|%.1i|\n", -42);
+	ft_printf("|%.2i|\n", -42);
+	ft_printf("|%1.i|\n", -42);
+	ft_printf("|%2.1i|\n", -42);
+	ft_printf("|%2.2i|\n", -42);
+	ft_printf("|%3.2i|\n", -42);
+	ft_printf("|%01i|\n", -42);
+	ft_printf("|%02.i|\n", -42);
+	ft_printf("|%02.1i|\n", -42);
+	ft_printf("|%02.2i|\n\n", -42);
+	ft_printf("|%+ i|\n", -42);
+	ft_printf("|%+ 1i|\n", -42);
+	ft_printf("|%+ 2i|\n", -42);
+	ft_printf("|%+ .i|\n", -42);
+	ft_printf("|%+ .1i|\n", -42);
+	ft_printf("|%.3i|\n", -42);
+	ft_printf("|%+ 1.i|\n", -42);
+	ft_printf("|%+ 2.1i|\n", -42);
+	ft_printf("|%+ 2.2i|\n", -42);
+	ft_printf("|%+ 3.2i|\n", -42);
+	ft_printf("|%+ 2.3i|\n", -42);
+	ft_printf("|%+ 01i|\n", -42);
+	ft_printf("|%+ 02i|\n", -42);
+	ft_printf("|%+ 02.1i|\n", -42);
+	ft_printf("|%+ 02.2i|\n\n",-42 );
+	
+	printf("Vrai printf : \n\n");
+	printf("|%-i|\n", -42);
+	printf("|%-1i|\n", -42);
+	printf("|%-2i|\n", -42);
+	printf("|%-.i|\n", -42);
+	printf("|%-.1i|\n", -42);
+	printf("|%-.2i|\n", -42);
+	printf("|%-1.i|\n", -42);
+	printf("|%-2.1i|\n", -42);
+	printf("|%-2.2i|\n", -42);
+	printf("|%-01i|\n", -42);
+	printf("|%-02.i|\n", -42);
+	printf("|%-02.1i|\n", -42);
+	printf("|%-02.2i|\n\n", -42);
+	
+	printf("|%-+ i|\n", -42);
+	printf("|%-+ 1i|\n", -42);
+	printf("|%-+ 2i|\n", -42);
+	printf("|%-+ .i|\n", -42);
+	printf("|%-+ .1i|\n", -42);
+	printf("|%-.3i|\n", -42);
+	printf("|%-+ 1.i|\n", -42);
+	printf("|%-+ 2.1i|\n", -42);
+	printf("|%-+ 2.2i|\n", -42);
+	printf("|%-+ 01i|\n", -42);
+	printf("|%-+ 02i|\n", -42);
+	printf("|%-+ 5i|\n", -42);
+	printf("|%-+ 02.1i|\n", -42);
+	printf("|%-+ 02.2i|\n\n", -42);
+
+	ft_printf("|%-i|\n", -42);
+	ft_printf("|%-1i|\n", -42);
+	ft_printf("|%-2i|\n", -42);
+	ft_printf("|%-.i|\n", -42);
+	ft_printf("|%-.1i|\n", -42);
+	ft_printf("|%-.2i|\n", -42);
+	ft_printf("|%-1.i|\n", -42);
+	ft_printf("|%-2.1i|\n", -42);
+	ft_printf("|%-2.2i|\n", -42);
+	ft_printf("|%-01i|\n", -42);
+	ft_printf("|%-02.i|\n", -42);
+	ft_printf("|%-02.1i|\n", -42);
+	ft_printf("|%-02.2i|\n\n", -42);
+
+	ft_printf("|%-+ i|\n", -42);
+	ft_printf("|%-+ 1i|\n", -42);
+	ft_printf("|%-+ 2i|\n", -42);
+	ft_printf("|%-+ .i|\n", -42);
+	ft_printf("|%-+ .1i|\n", -42);
+	ft_printf("|%-.3i|\n", -42);
+	ft_printf("|%-+ 1.i|\n", -42);
+	ft_printf("|%-+ 2.1i|\n", -42);
+	ft_printf("|%-+ 2.2i|\n", -42);
+	ft_printf("|%-+ 01i|\n", -42);
+	ft_printf("|%-+ 02i|\n", -42);
+	ft_printf("|%-+ 5i|\n", -42);
+	ft_printf("|%-+ 02.1i|\n", -42);
+	ft_printf("|%-+ 02.2i|\n\n", -42);
+*/
 	return 0;
 }
--- srcs/print_d.c
+++ srcs/print_d.c
@@ -6,99 +6,118 @@
 /*   By: nerahmou <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/05 22:01:35 by nerahmou     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/23 10:17:41 by nerahmou    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/24 11:07:37 by nerahmou    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../include/ft_printf.h"
 
-static void		fill_plus_min(int is_neg, t_suitcase *s_c, int nb)
+static void	print_d_min(intmax_t nbr, int nbr_len, int is_neg, t_suitcase *s_c)
 {
-	if (is_neg)
+	if (s_c->is_plus || is_neg)
 	{
-		s_c->ret += ft_putchar('-');
-		s_c->width--;
-	}
-	else if (s_c->is_space || s_c->is_plus)
-	{
-		if (s_c->is_space && !s_c->prec && !s_c->is_plus)
-			ft_putchar(' ');
-		else if (s_c->is_plus)
-			ft_putchar('+');
+		if (s_c->is_plus && !is_neg)
+			s_c->ret += ft_putchar('+');
 		else
-			return ;
-		s_c->ret++;
+			s_c->ret += ft_putchar('-');
 		s_c->width--;
 	}
-	else if (s_c->is_zero && !s_c->is_minus && s_c->width > nbrlen(nb, 10))
-		(s_c->ret += ft_putchar('0')) && s_c->width--;
-}
-
-static	void	print_no_mp(intmax_t nb, int nb_l, int is_neg, t_suitcase *s_c)
-{
-	if (s_c->is_zero)
+	if (s_c->is_precision)
 	{
-		fill_plus_min(is_neg, s_c, nb);
-		s_c->width++;
-	}
-	while (s_c->width > nb_l + (is_neg || s_c->is_zero ||
-				(s_c->is_plus && nb) ? 1 : 0))
-	{
-		s_c->ret += s_c->is_zero || (!nb && s_c->prec) ?
-			ft_putchar('0') : ft_putchar(' ');
-		s_c->width--;
-	}
-	if (!s_c->is_zero)
-		fill_plus_min(is_neg, s_c, nb);
-	if (!nb && s_c->is_precision && !s_c->prec)
-		while (s_c->width-- > 0)
-			s_c->ret += !nb && s_c->prec ? ft_putchar('0') : ft_putchar(' ');
-	else
-	{
-		ft_putnbr_base(nb, DEC, 10);
-		s_c->ret += nb_l;
-	}
-}
-
-static	void	print_d_nom(intmax_t nb, int nb_l, int is_neg, t_suitcase *s_c)
-{
-	if (s_c->prec > nb_l)
-	{
-		while (s_c->width-- > s_c->prec + (s_c->is_plus || is_neg) ? 1 : 0)
-			s_c->ret += ft_putchar(' ');
-		fill_plus_min(is_neg, s_c, nb);
-		while (s_c->prec > nb_l)
+		while (s_c->prec > nbr_len)
 		{
 			s_c->ret += ft_putchar('0');
 			s_c->prec--;
+			s_c->width--;
 		}
-		ft_putnbr_base(nb, DEC, 10);
-		s_c->ret += nb_l;
+		if (!s_c->prec && !nbr)
+		{
+			while (s_c->width-- > 0)
+				s_c->ret += ft_putchar(' ');
+		}
+		else
+		{
+			ft_putnbr_base(nbr, DEC, 10);
+			s_c->ret += nbr_len;
+			s_c->width -= nbr_len;
+			while (s_c->width-- > 0)
+				s_c->ret += ft_putchar(' ');
+		}
 	}
 	else
-		print_no_mp(nb, nb_l, is_neg, s_c);
+	{
+		ft_putnbr_base(nbr, DEC, 10);
+		s_c->ret += nbr_len;
+		s_c->width -= nbr_len;
+		while (s_c->width-- > 0)
+			s_c->ret += ft_putchar(' ');
+	}
 }
 
-static void		print_d_min(intmax_t nb, int nb_l, int is_neg, t_suitcase *s_c)
+static void	print_d_nom(intmax_t nbr, int nbr_len, int is_neg, t_suitcase *s_c)
 {
-	fill_plus_min(is_neg, s_c, nb);
-	while (s_c->prec > nb_l)
+	if (s_c->is_space && !s_c->is_plus && !is_neg)
 	{
-		s_c->ret += ft_putchar('0');
+		s_c->ret += ft_putchar(' ');
+		is_neg = 0;
 		s_c->width--;
-		s_c->prec--;
 	}
-	if (s_c->is_precision && !s_c->prec && !nb)
-		s_c->width++;
+	if (s_c->is_plus && !is_neg && s_c->prec <= nbr_len)
+	{
+		s_c->ret += ft_putchar('+');
+		s_c->width--;
+		s_c->is_plus = 0;
+	}
+	if (s_c->is_precision)
+	{	
+		while (s_c->width > s_c->prec + (is_neg || s_c->is_plus) && s_c->width > nbr_len + (is_neg || s_c->is_plus))
+		{
+			s_c->ret += ft_putchar(' ');
+			s_c->width--;
+		}
+		if (!s_c->prec && !nbr)
+		{
+			while (s_c->width-- > 0)
+				s_c->ret += ft_putchar(' ');
+		}
+		else
+		{
+			if (s_c->is_plus || is_neg)
+			{
+				if (s_c->is_plus && !is_neg)
+					s_c->ret += ft_putchar('+');
+				else
+					s_c->ret += ft_putchar('-');
+				s_c->width--;
+			}
+			while (s_c->prec-- > nbr_len)
+			{
+				s_c->ret += ft_putchar('0');
+
+			}
+			ft_putnbr_base(nbr, DEC, 10);
+			s_c->ret += nbr_len;
+		}
+	}
 	else
 	{
-		ft_putnbr_base(nb, DEC, 10);
-		s_c->ret += nb_l;
+		if (is_neg && s_c->is_zero && s_c->width > nbr_len)
+		{
+			s_c->ret += ft_putchar('-');
+			s_c->width--;
+			is_neg = 0;
+		}
+		while (s_c->width > nbr_len + is_neg)
+		{
+			s_c->ret += s_c->is_zero ? ft_putchar('0') : ft_putchar(' ');
+			s_c->width--;
+		}
+		if (is_neg)
+			s_c->ret += ft_putchar('-');
+		ft_putnbr_base(nbr, DEC, 10);
+		s_c->ret += nbr_len;
 	}
-	s_c->width -= nb_l;
-	while (s_c->width-- > 0)
-		s_c->ret += ft_putchar(' ');
 }
 
 void			print_d(va_list *ap, t_suitcase *s_c)
